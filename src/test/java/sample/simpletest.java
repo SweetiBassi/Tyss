1074988d95dc3ca784b09bc0776b41b1efd7c9ed
SweetiBassi <er.sweeti1987@gmail.com>
2022-07-25 15:28:48 +0530

    233

--- src/test/java/sample/simpletest.java
+++ src/test/java/sample/simpletest.java
@@ -5,9 +5,8 @@ public class simpletest {
 	public static void main(String[] args) {
 System.out.println("hiiiiiiiiii");
 System.out.println("byeee");
+System.out.println("hello");
 	}
 //added something
-	//byeeeehjdvsjdsjv
+
 }
-//frgkdtdsg
-//byee
