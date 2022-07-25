79a7bf4dd65759e76627ac324b854b5acb170f91
SweetiBassi <er.sweeti1987@gmail.com>
2022-07-25 12:56:20 +0530

    erty

---
+++ src/test/java/sample/simpletest.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class simpletest {
+
+	public static void main(String[] args) {
+System.out.println("hiiiiiiiiii");
+System.out.println("byeee");
+	}
+//added something
+	//byeeeehjdvsjdsjv
+}
+//frgkdt
