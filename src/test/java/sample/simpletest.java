0a58ea1d24619b46fcc09bfbc482b5255e46d1ab
SweetiBassi <er.sweeti1987@gmail.com>
2022-07-25 15:35:27 +0530

    last commit

--- src/test/java/sample/simpletest.java
+++ src/test/java/sample/simpletest.java
@@ -5,7 +5,7 @@ public class simpletest {
 	public static void main(String[] args) {
 System.out.println("hiiiiiiiiii");
 System.out.println("byeee");
-System.out.println("hello");
+System.out.println("hello34");
 	}
 //added something
 
